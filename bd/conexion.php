8306bd55239673ab30739b1e4f3b3c36bf14d550
Angel-Nevescanin <angel.nevescanin244321@potros.itson.edu.mx>
2025-03-10 02:05:36 -0700

    LocalHost
    Configurado para abrir en local.

--- bd/conexion.php
+++ bd/conexion.php
@@ -3,10 +3,10 @@ class Conexion
 {
     public static function Conectar()
     {
-        define('servidor', '127.0.0.2');
+        define('servidor', '127.0.0.1');
         define('nombre_bd', 'selectadb');
         define('usuario', 'root');
-        define('password', '');
+        define('password', 'root');
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
         try {
             $conexion = new PDO("mysql:host=" . servidor . ";dbname=" . nombre_bd, usuario, password, $opciones);
